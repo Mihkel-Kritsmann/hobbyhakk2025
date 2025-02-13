0238cfaa633fb8e51d5261049e7c6accc894bc25
Mihkel-Kritsmann <mihkelkritsmann@gmail.com>
2025-02-13 22:54:03 +0200

    remove Translate service

--- apis/llm.py
+++ apis/llm.py
@@ -1,11 +1,14 @@
 # services/llm_service.py
 from transformers import pipeline
-
+import gc
+import torch
+gc.collect()
+torch.cuda.empty_cache()
 
 class LLM:
     def __init__(self):
         # Initialize local model
-        self.pipe = pipeline("image-text-to-text", model="unsloth/Llama-3.2-11B-Vision-unsloth-bnb-4bit")
+        self.pipe = pipeline("image-text-to-text", model="unsloth/Llama-3.2-11B-Vision-Instruct-unsloth-bnb-4bit")
     
     def generate(self, text):
         response = self.pipe(text)
--- app.py
+++ app.py
@@ -2,15 +2,14 @@
 from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
 from apis.llm import LLM
-from apis.translate import Translate
 from apis.neurokone import Neurokone
 
-
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 # Initialize services
 llm_service = LLM()
-translate_service = Translate()
 speech_service = Neurokone()
 
 @app.route('/')
@@ -22,26 +21,20 @@ def process():
     try:
         data = request.json
         text = data.get('text', '')
-        target_language = data.get('target_language', 'en')
         speaker = data.get('speaker', 'mari')  # Default speaker
         
         # Process with LLM
         llm_response = llm_service.generate(text)
         
-        # Translate into estonian
-        
-        #translated_text = translate_service.translate( llm_response, target_language)
-        
         # Generate speech
         audio_file = speech_service.text_to_speech(
-            "Tere minu nimi on Mari",
+            llm_response,
             speaker=speaker
         )
         
         return jsonify({
             'original_text': text,
             'llm_response': llm_response,
-            'translated_text': "Not working",
             'audio_file': audio_file
         })
     
